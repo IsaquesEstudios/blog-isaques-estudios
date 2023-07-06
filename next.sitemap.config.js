574603cfcf035133a48a0f9dd716aa326d41c181
Matteus Isaque <matteus.isaque28@gmail.com>
2023-07-06 18:00:09 -0300

    alt

--- next.sitemap.config.js
+++ next.sitemap.config.js
@@ -1,10 +1,10 @@
-// module.exports = {
-//   siteUrl: 'https://teste.isaquesestudios.com',
-//   generateRobotsTxt: true,
-//   exclude: ['/server-sitemap.xml'], // <= exclude here
-//   robotsTxtOptions: {
-//     additionalSitemaps: [
-//       'https://teste.isaquesestudios.com/server-sitemap.xml', // <==== Add here
-//     ],
-//   },
-// }
+module.exports = {
+  siteUrl: 'https://teste.isaquesestudios.com',
+  generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'], // <= exclude here
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://teste.isaquesestudios.com/server-sitemap.xml', // <==== Add here
+    ],
+  },
+}
