c23e603b5914cbfcc3565c29168fe8b7d19f3069
Matteus Isaque <matteus.isaque28@gmail.com>
2023-07-30 09:20:42 -0300

    alt

--- next.sitemap.config.js
+++ next.sitemap.config.js
@@ -1,13 +1,13 @@
-// module.exports = {
-//   siteUrl: 'https://teste.isaquesestudios.com',
-//   generateRobotsTxt: true,
-//   exclude: ['/server-sitemap.xml'], // <= exclude here
-//   robotsTxtOptions: {
-//     additionalSitemaps: [
-//       'https://teste.isaquesestudios.com/server-sitemap.xml', // <==== Add here
-//     ],
-//   },
-// }
+module.exports = {
+  siteUrl: 'https://blog.isaquesestudios.com',
+  generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'], // <= exclude here
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://blog.isaquesestudios.com/server-sitemap.xml', // <==== Add here
+    ],
+  },
+}
 
 
-// "postbuild": "next-sitemap --config next.sitemap.config.js",
+
